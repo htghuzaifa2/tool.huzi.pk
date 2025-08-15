5036927926819509f76740cd697f1239c844c888
HTG HUZAIFA <htghuzaifa2@gmail.com>
2025-08-15 21:14:59 +0000

    likw you tried to fix it so many times how this time i will able to depl

--- src/app/html-table-generator/page.tsx
+++ src/app/html-table-generator/page.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import { useState, useMemo } from 'react';
@@ -5,7 +6,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Checkbox } from '@/components/ui/checkbox';
-import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { useToast } from "@/hooks/use-toast";
 import { Copy, Table, Code } from 'lucide-react';
 
